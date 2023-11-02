f70acf41cdf3470039c1716e785c68b5f3ff2a95
Daryna Saiun <dashasaiun@gmail.com>
2023-11-02 12:38:05 +0200

    changed model.py and RGR_KV-13_Saiun.pdf

--- model.py
+++ model.py
@@ -83,11 +83,13 @@ class Model:
         if not table_exists:
             c.execute('''
                 CREATE TABLE "Publishing" (
-                    "AuthorID" character varying COLLATE pg_catalog."default" NOT NULL,
-                    "PublicationID" character varying COLLATE pg_catalog."default" NOT NULL,
-                    "ISSN" integer NOT NULL,
-                    "Date" date NOT NULL,
-                    CONSTRAINT "Publishing_pkey" PRIMARY KEY ("AuthorID", "PublicationID", "ISSN")
+                    "AuthorID" character varying,
+                    "PublicationID" character varying,
+                    "ISSN" integer,
+                    "date" date,
+                    CONSTRAINT "Publishing_AuthorID_fkey" FOREIGN KEY ("AuthorID") REFERENCES "Author" ("AuthorID"),
+                    CONSTRAINT "Publishing_PublicationID_fkey" FOREIGN KEY ("PublicationID") REFERENCES "Publication" ("PublicationID"),
+                    CONSTRAINT "Publishing_ISSN_fkey" FOREIGN KEY ("ISSN") REFERENCES "Collection" ("ISSN")
                 )
             ''')
 
@@ -108,7 +110,10 @@ class Model:
             self.view.show_message("Added successfully!")
         except psycopg2.Error as e:
             self.conn.rollback()
-            print(e)
+            if "duplicate key value" in str(e):
+                self.view.show_message(f"ПОМИЛКА: дані з таким ключом вже існують.\nКод помилки: {e.pgcode}.")
+            elif "не присутній в таблиці" in str(e):
+                self.view.show_message(f"ПОМИЛКА: немає референсу на дані з батьківських таблиць. \nКод помилки: {e.pgcode}.")
 
     def update_data(self, table_name, data, condition_column, condition_value):
         try:
@@ -140,9 +145,12 @@ class Model:
             c.execute(sql)
             self.conn.commit()
             self.view.show_message("Deleted successfully!")
-        except psycopg2.Error as e:
+        except psycopg2.IntegrityError as e:
             self.conn.rollback()
-            print(e)
+            if "все ще є посилання" in str(e):
+                self.view.show_message(f"ПОМИЛКА: видаліть спочатку з \"Publishing\" записи з \"{cond}\" = '{val}'\nПісля чого спробуйте ще раз.")
+            else:
+                self.view.show_message(f"ПОМИЛКА: не знайдені відповідні записи \"{cond}\" = '{val}'")
 
     def delete_data_publishing(self, table_name, cond1, val1, cond2, val2, cond3, val3):
         try:
